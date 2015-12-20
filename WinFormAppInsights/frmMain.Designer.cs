7dc2082553dbdfe7fc4559cd19324b8788e12d2f
maduka <maduka_bor@hotmail.com>
2015-12-20 23:37:41 +0800

    Lab Code Finish

---
+++ WinFormAppInsights/frmMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WinFormAppInsights
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnException = new System.Windows.Forms.Button();
+            this.btnFlush = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(35, 32);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(119, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "送出Button事件";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnException
+            // 
+            this.btnException.Location = new System.Drawing.Point(35, 71);
+            this.btnException.Name = "btnException";
+            this.btnException.Size = new System.Drawing.Size(119, 23);
+            this.btnException.TabIndex = 1;
+            this.btnException.Text = "發生例外事件";
+            this.btnException.UseVisualStyleBackColor = true;
+            this.btnException.Click += new System.EventHandler(this.btnException_Click);
+            // 
+            // btnFlush
+            // 
+            this.btnFlush.Location = new System.Drawing.Point(35, 110);
+            this.btnFlush.Name = "btnFlush";
+            this.btnFlush.Size = new System.Drawing.Size(119, 23);
+            this.btnFlush.TabIndex = 2;
+            this.btnFlush.Text = "Flush";
+            this.btnFlush.UseVisualStyleBackColor = true;
+            this.btnFlush.Click += new System.EventHandler(this.btnFlush_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 145);
+            this.Controls.Add(this.btnFlush);
+            this.Controls.Add(this.btnException);
+            this.Controls.Add(this.btnSend);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnException;
+        private System.Windows.Forms.Button btnFlush;
+    }
+}
+
---
+++ WinFormAppInsights/frmMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ApplicationInsights;
+
+namespace WinFormAppInsights
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private TelemetryClient objTc = new TelemetryClient();
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            objTc.InstrumentationKey = "請在這裡放入金鑰";
+
+            objTc.Context.User.Id = Environment.UserName;
+            objTc.Context.Session.Id = Guid.NewGuid().ToString();
+            objTc.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+            objTc.TrackPageView("frmMain");
+        }
+
+        /// <summary>
+        /// 單純的透過Button送出Application Insights的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            objTc.TrackEvent("Button送出的事件");
+            MessageBox.Show("已送出事件");
+        }
+
+        /// <summary>
+        /// 刻意發生意外事件的動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnException_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable objDt = new DataTable();
+                string strColumnValue = objDt.Rows[0]["Column1"].ToString();
+            }
+            catch (Exception ex)
+            {
+                objTc.TrackException(ex);
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+            }
+        }
+
+        private void btnFlush_Click(object sender, EventArgs e)
+        {
+            objTc.Flush();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            objTc.Flush();
+        }
+    }
+}
